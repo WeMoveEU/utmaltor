160a2c97438dacad475ce5836480cfc9f5fcbbd9
scardinius <scardinius@chords.pl>
2017-07-27 17:02:40 +0200

    Add tests

--- tests/phpunit/CRM/Utmaltor/AlterTest.php
+++ tests/phpunit/CRM/Utmaltor/AlterTest.php
@@ -31,6 +31,7 @@ class CRM_Utmaltor_AlterTest extends \PHPUnit_Framework_TestCase
   }
 
   public function setUp() {
+    $this->settings();
     $mailingId = 1000;
     $params = [
       'id' => $mailingId,
@@ -38,9 +39,19 @@ class CRM_Utmaltor_AlterTest extends \PHPUnit_Framework_TestCase
       'campaign_lang' => '',
     ];
     $this->utmSmarty = \CRM_Utmaltor_Logic_Smarty::singleton($params);
-    $this->source = Civi::settings()->get('utmaltor_source');
-    $this->medium = Civi::settings()->get('utmaltor_medium');
-    $this->campaign = Civi::settings()->get('utmaltor_campaign');
+  }
+
+  private function settings() {
+    $this->source = 'civimail-{$mailing_id}';
+    $this->medium = 'email';
+    $this->campaign = '{$date|date_format:"%Y%m%d"}{if $campaign_lang}_{$campaign_lang}{/if}';
+    Civi::settings()->set('utmaltor_domains', 'wemove.eu');
+    Civi::settings()->set('utmaltor_source', $this->source);
+    Civi::settings()->set('utmaltor_medium', $this->medium);
+    Civi::settings()->set('utmaltor_campaign', $this->campaign);
+    Civi::settings()->set('utmaltor_source_override', 1);
+    Civi::settings()->set('utmaltor_medium_override', 1);
+    Civi::settings()->set('utmaltor_campaign_override', 0);
   }
 
   public function testAlterCleanUrl() {
@@ -52,6 +63,47 @@ class CRM_Utmaltor_AlterTest extends \PHPUnit_Framework_TestCase
       $this->utmSmarty->parse($this->medium),
       '&utm_campaign=',
       $this->utmSmarty->parse($this->campaign),
+    ]);
+    $actualUrl = \CRM_Utmaltor_Logic_Alter::url($url, $this->utmSmarty);
+    $this->assertEquals($expectedUrl, $actualUrl);
+  }
+
+  public function testAlterUrlWithOverratedMedium() {
+    $url = 'https://www.wemove.eu/test?utm_medium=MEDIUM';
+    $expectedUrl = implode('', [
+      'https://www.wemove.eu/test?utm_medium=',
+      $this->utmSmarty->parse($this->medium),
+      '&utm_source=',
+      $this->utmSmarty->parse($this->source),
+      '&utm_campaign=',
+      $this->utmSmarty->parse($this->campaign),
+    ]);
+    $actualUrl = \CRM_Utmaltor_Logic_Alter::url($url, $this->utmSmarty);
+    $this->assertEquals($expectedUrl, $actualUrl);
+  }
+
+  public function testAlterUrlWithOverratedSource() {
+    $url = 'https://www.wemove.eu/test?utm_source=SOURCE';
+    $expectedUrl = implode('', [
+      'https://www.wemove.eu/test?utm_source=',
+      $this->utmSmarty->parse($this->source),
+      '&utm_medium=',
+      $this->utmSmarty->parse($this->medium),
+      '&utm_campaign=',
+      $this->utmSmarty->parse($this->campaign),
+    ]);
+    $actualUrl = \CRM_Utmaltor_Logic_Alter::url($url, $this->utmSmarty);
+    $this->assertEquals($expectedUrl, $actualUrl);
+  }
+
+  public function testAlterUrlWithCustomCampaign() {
+    $url = 'https://www.wemove.eu/test?utm_campaign=CAMPAIGN';
+    $expectedUrl = implode('', [
+      'https://www.wemove.eu/test?utm_campaign=CAMPAIGN',
+      '&utm_source=',
+      $this->utmSmarty->parse($this->source),
+      '&utm_medium=',
+      $this->utmSmarty->parse($this->medium),
     ]);
     $actualUrl = \CRM_Utmaltor_Logic_Alter::url($url, $this->utmSmarty);
     $this->assertEquals($expectedUrl, $actualUrl);
